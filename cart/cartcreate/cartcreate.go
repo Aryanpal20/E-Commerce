fe10d86e8b2e14fe3150c9e67191e79b37af82f3
aryan <aryanpal692@gmail.com>
2023-01-24 10:37:45 +0530

    with login

--- cart/cartcreate/cartcreate.go
+++ cart/cartcreate/cartcreate.go
@@ -1,6 +1,7 @@
 package cartcreate
 
 import (
+	email "e-Commerce/FetchOnlyOneData/fetchemail"
 	role "e-Commerce/FetchOnlyOneData/fetchrole"
 	cart "e-Commerce/Models/Cart_Model"
 	pro "e-Commerce/Models/Product_Model"
@@ -16,7 +17,7 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	token := strings.Split(r.Header["Token"][0], " ")[1]
 	role := role.Is_manager(token)
-
+	email := email.Task_creator(token)
 	if role == "customer" {
 		var cart cart.Cart
 		var user user.User
@@ -26,19 +27,25 @@ func AddToCart(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(pro)
 		database.Database.Where("id = ?", cart.Userid).Find(&user)
 		fmt.Println(user)
-		cart.Select_Product_Name = pro.Product_Name
-		cart.Quantity_wise_Rate = pro.Product_Rate
-		cart.Userid = user.ID
-		if cart.Select_Product_Quantity <= pro.Product_Quantity {
-			if cart.Select_Product_Quantity > 1 {
-				rate := pro.Product_Rate * cart.Select_Product_Quantity
-				cart.Quantity_wise_Rate = rate
+		if user.Email == email {
+			cart.Select_Product_Name = pro.Product_Name
+			cart.Quantity_wise_Rate = pro.Product_Rate
+			cart.Userid = user.ID
+			if cart.Select_Product_Quantity <= pro.Product_Quantity {
+				if cart.Select_Product_Quantity > 1 {
+					rate := pro.Product_Rate * cart.Select_Product_Quantity
+					cart.Quantity_wise_Rate = rate
+				}
+			} else {
+				cart.Select_Product_Quantity = pro.Product_Quantity
 			}
+			database.Database.Create(&cart)
+			json.NewEncoder(w).Encode(cart)
 		} else {
-			cart.Select_Product_Quantity = pro.Product_Quantity
+			err := "you can enter your id"
+			json.NewEncoder(w).Encode(err)
 		}
-		database.Database.Create(&cart)
-		json.NewEncoder(w).Encode(cart)
+
 	}
 
 }
